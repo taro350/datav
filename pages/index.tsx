17c8f40e9d6bb5a0bb388488c55be9f814b0b4f0
Taro Saika <tarosaika@Taros-MacBook-Pro.local>
2022-08-02 23:33:58 +0900

    some progress

---
+++ pages/index.tsx
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+
+
+
+export default function Home() {
+  return (
+
+    <>
+    <Head>
+      <metadata type='UTF8'></metadata>
+    </Head>
+    
+    <div>
+        <div className='top-banner'>
+
+          <div className='wrapper'>
+            <h1> List </h1>
+
+          </div>
+
+
+          <footer className=''>
+
+          </footer>
+        </div>
+      </div>
+    </>
+  )
+}
